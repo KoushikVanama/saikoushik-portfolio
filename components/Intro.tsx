6c0617c53052a4ff1c988016d585a7137221271b
Koushik <koushik.vanama@gmail.com>
2023-12-01 01:38:13 +0530

    eslint build error fixes

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -5,9 +5,9 @@ const Intro = () => {
         <div className="w-full h-full flex flex-col justify-center items-center">
             <div className="flex flex-col p-10">
                 <h1 className="lg:text-4xl text-3xl font-extrabold text-white">
-                    Hello, I &apos;m <span className="text-heading-color hover:bg-white">Sai Venkata Koushik Vanama</span>.
+                    Hello, I am <span className="text-heading-color hover:bg-white">Sai Venkata Koushik Vanama</span>.
                 </h1>
-                <h3 className="lg:text-4xl text-3xl py-2 font-bold text-white">I&apos;m a full stack developer.</h3>
+                <h3 className="lg:text-4xl text-3xl py-2 font-bold text-white">I am a full stack developer.</h3>
             </div>
             <a href="#about" className="flex justify-center p-2 my-5 text-center border-2 hover:bg-primary-color border-white w-[8rem] h-[3rem] text-white">
                 About me
